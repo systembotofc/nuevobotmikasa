6d8186781b840b9490b569ef72933daa18155fb0
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-10 17:20:35 -0400

    Actualizar Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -1,39 +1,22 @@
-const cooldown = 60000
+import axios from 'axios'
+import { apivisit } from './kanghit.js'
 
-let handler = async (m, { conn, args, usedPrefix, isPrems, isOwner, command }) => {
-    if (m.chat.includes('120363041604217979') && !isOwner) return m.reply(`[ hehe ]`)
-    if (!args[0]) return m.reply(`Format : ${usedPrefix + command} <timer>\n1 = 1 menit\n5 = 5 menit ... dst.\n\nContoh : *${usedPrefix + command} 10*`)
-    const item = (args[0] || '').toLowerCase()
-    const total = Math.floor(isNumber(args[0]) ? Math.min(Math.max(parseInt(args[0]), 1), Number.MAX_SAFE_INTEGER) : 1) * 1
-    if (total > 20 && !isPrems) return m.reply(`_... >> not premium ..._\n[!] Maksimal ${command} : 20 menit.`)
-    if (total > 70 && !isOwner) return m.reply(`[!] Maksimal ${command} : 70 menit.`)
-    let chat = global.db.data.chats[m.chat]
-    chat.isBanned = true
-    m.reply(`Bot senyap selama ${total} menit!`)
-    setTimeout(() => {
-        chat.isBanned = false
-    }, cooldown * total)
-    setTimeout(() => {
-        chat.isBanned = false
-        m.reply(`Bot dapat digunakan kembali.`)
-    }, (cooldown * total) + 2000)
-    chat.lastmute = new Date * 1 + (cooldown * total)
-}
-
-handler.help = ['mute']
-handler.tags = ['group']
-handler.command = /^(mute|senyap)$/i
-
-handler.admin = true
-handler.botAdmin = true
-handler.group = true
-
-handler.cooldown = cooldown
-
+let handler = async (m, { conn, args }) => {
+    if (!args[0]) throw m.reply('Putting *URL* Cocofun...')
+    if (!args[0].includes("cocofun")) return m.reply(`_Invalid Url..._`)
+    let res = (await axios.get(API('can', '/api/download/cocofun', { url: args[0] } ))).data;
+    if (res.status != 200) throw res.message;
+    if (!res) throw res.message;
+        await m.reply('Sedang diproses...')
+    await conn.sendMessage(m.chat, { video: { url: res?.result?.no_watermark }, caption: res?.result?.caption}, { quoted: m })
+    await apivisit
+        // By Chandra XD
+        // Follow bang
+        // TikTok : @pnggilajacn
+        // Github : https://github.com/Chandra-XD
+        }
+handler.help = ['cocofun'].map(v => v + ' <url>')
+handler.tags = ['downloader', 'premium']
+handler.command = /^(cocofun|cocofundl)$/i
+handler.premium = true
 export default handler
-
-function isNumber(number) {
-    if (!number) return number
-    number = parseInt(number)
-    return typeof number == 'number' && !isNaN(number)
-}
