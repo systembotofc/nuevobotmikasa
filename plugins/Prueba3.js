28ddb9aa773d1ba13e38638cc80ba441f6fa5d0a
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-10 18:10:31 -0400

    Actualizar Prueba3.js

--- plugins/Prueba3.js
+++ plugins/Prueba3.js
@@ -1,17 +1,16 @@
 let handler = async (m, { conn, text }) => {
-if (!text) return m.reply('🚩 Ingresa un nombre para el grupo.')
+if (!text) return m.reply('🐈‍⬛ Ingresa un nombre para el grupo.')
 try{
-m.reply('🚩 *Creando grupo*')
+m.reply('🐈‍⬛ *Creando grupo*')
 let group = await conn.groupCreate(text, [m.sender])
 let link = await conn.groupInviteCode(group.gid)
 m.reply('https://chat.whatsapp.com/' + url)
 } catch (e) {
-m.reply(`🚩 Ocurrió un error.`)
+m.reply(`🐈‍⬛ Ocurrió un error.`)
 }
 }
 handler.help = ['grupocrear <nombre>']
 handler.tags = ['mods']
 handler.command = ['creargc', 'newgc', 'creargrupo', 'grupocrear']
-handler.mods = true
 handler.register = true
 export default handler
