382db5f7d83d7ff5df0b1ba14417cab1bbdef151
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-11 15:59:47 -0400

    Actualizar plugins

---
+++ plugins/new-creargrupo.js
@@ -0,0 +1,16 @@
+let handler = async (m, { conn, text }) => {
+if (!text) return m.reply('🐈‍⬛ Ingresa un nombre para el grupo.')
+try{
+m.reply('🐈‍⬛ *Creando grupo*')
+let group = await conn.groupCreate(text, [m.sender])
+let link = await conn.groupInviteCode(group.gid)
+m.reply('https://chat.whatsapp.com/' + url)
+} catch (e) {
+m.reply(`🐈‍⬛ Ocurrió un error.`)
+}
+}
+handler.help = ['creargrupo <nombre>']
+handler.tags = ['new']
+handler.command = ['creargc', 'newgc', 'creargrupo', 'grupocrear']
+handler.register = true
+export default handler
