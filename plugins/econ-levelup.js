913d8ab8ec98112dd8f4e46b1047f70477c22e27
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-11 18:00:05 -0400

    Actualizar econ-levelup.js

--- plugins/econ-levelup.js
+++ plugins/econ-levelup.js
@@ -2,7 +2,7 @@
 import { canLevelUp, xpRange } from '../lib/levelling.js'
 let handler = async (m, { conn }) => {
 	let name = conn.getName(m.sender)
-  let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.ibb.co/jHctydb/Genesis-Bot.jpg')
+  let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.ibb.co/vD9Bhd3/file.jpg')
     let user = global.db.data.users[m.sender]
     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
         let { min, xp, max } = xpRange(user.level, global.multiplier)
