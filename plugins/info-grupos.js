c24ed1294083de44c7db362d4f4d596516c0abd4
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-11 17:35:07 -0400

    Actualizar info-grupos.js

--- plugins/info-grupos.js
+++ plugins/info-grupos.js
@@ -1,6 +1,6 @@
 
 let handler = async (m, { conn, command, usedPrefix }) => {
-let pp = 'https://i.ibb.co/Wxgkxm7/file.jpg'
+let pp = 'https://i.ibb.co/vD9Bhd3/file.jpg'
 m.react('✅')
 let name = await conn.getName(m.sender)
 let _uptime = process.uptime() * 1000
