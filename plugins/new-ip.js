11e48b2fba495810822e211941824f7c21e33a94
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-11 21:35:49 -0400

    Actualizar new-ip.js

--- plugins/new-ip.js
+++ plugins/new-ip.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
 //await m.reply('🧑🏻‍💻 Buscando...')
 let bot = '🐈‍⬛ `𝗖𝗔𝗥𝗚𝗔𝗡𝗗𝗢....`'
 conn.reply(m.chat, bot, m, fwc, )
-  if (!text) return conn.reply(m.chat, '🚩 *Te Faltó La <Ip>*', m, rcanal, )
+  if (!text) return conn.reply(m.chat, '🐈‍⬛ *Te Faltó La <Ip>*', m, rcanal, )
 
   axios.get(`http://ip-api.com/json/${text}?fields=status,message,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,mobile,hosting,query`).then ((res) => {
     const data = res.data
