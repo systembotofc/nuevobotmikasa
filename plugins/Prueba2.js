ebc52b7e64c8b1136cce3f85f614d425aa1a117a
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-13 20:22:12 -0400

    Actualizar Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, text, usedPrefix, command: cmd }) => {
         let format = PhoneNum(`+${num.split('@')[0]}`)
         let country = regionNames.of(format.getRegionCode('international'))
         let wea = `\t*WHATSAPP*\n\n*País:* ${country.toUpperCase()}\n*Nombre:* ${name ? name : '-'}\n*Formato del numero:* ${format.getNumber('international')}\n*Link:* wa.me/${num.split('@')[0]}\n*Mencion:* @${num.split('@')[0]}\n*Estado:* ${bio?.status || '-'}\n*Datos del estado:* ${bio?.setAt ? moment(bio.setAt.toDateString()).locale('id').format('LL') : '-'}\n\n${business ? `\t*Informacion de empresa*\n\n*Id de la empresa:* ${business.wid}\n*Sitio web:* ${business.website ? business.website : '-'}\n*Email:* ${business.email ? business.email : '-'}\n*Categoria:* ${business.category}\n*Direccion:* ${business.address ? business.address : '-'}\n*Tiempo:* ${business.business_hours.timezone ? business.business_hours.timezone : '-'}\n*Descripcion*: ${business.description ? business.description : '-'}` : ''}`
-        img ? await conn.sendMessage(m.chat, { image: { url: img }, caption: wea, mentions: [num] }, { quoted: m, fwc }) : m.reply(wea)
+        img ? await conn.sendMessage(m.chat, { image: { url: img }, caption: wea, mentions: [num] }, { quoted: m }) : m.reply(wea)
 }
 
 handler.help = ['infowa']
