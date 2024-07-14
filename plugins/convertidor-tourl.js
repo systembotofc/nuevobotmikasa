7912b4ee312d9c89b2d19306e65177c149c4fd6e
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-13 20:15:45 -0400

    Actualizar convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -1,16 +1,44 @@
-import uploadFile from '../lib/uploadFile.js';
-import uploadImage from '../lib/uploadImage.js';
-const handler = async (m) => {
-  const q = m.quoted ? m.quoted : m;
-  const mime = (q.msg || q).mimetype || '';
-  if (!mime) throw '${mssg.avisoGene3}\n\n*RESPONDA A UNA IMAGEN O VIDEO PARA HACERLO URL*';
-m.react('✅')
-  const media = await q.download();
-  const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
-  const link = await (isTele ? uploadImage : uploadFile)(media);
-  m.reply(`${mssg.avisoGene2}\n\n*ENLACE DE SU ARCHIVO:*\n${link}`);
-};
-handler.help = ['tourl <reply image>'];
-handler.tags = ['convertir'];
-handler.command = /^(upload|tourl)$/i;
-export default handler;
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+import fetch from 'node-fetch'
+
+let handler = async (m) => {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) return conn.reply(m.chat, '${mssg.avisoGene3}\n\n*RESPONDA A UNA IMAGEN O VIDEO PARA HACERLO URL*', m, rcanal)
+  await m.react('🕓')
+  try {
+  let media = await q.download()
+  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+  let link = await (isTele ? uploadImage : uploadFile)(media)
+  let img = await (await fetch(`${link}`)).buffer()
+  let txt = ``
+      txt += `  *Enlace* : ${link}\n`
+      txt += `  *Acortado* : ${await shortUrl(link)}\n`
+      txt += `  *Tamaño* : ${formatBytes(media.length)}\n`
+      txt += `  *Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`
+      txt += ``
+
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
+await m.react('✅')
+} catch {
+await m.react('✖️')
+}}
+handler.help = ['tourl']
+handler.tags = ['tools']
+handler.command = /^(tourl|upload)$/i
+export default handler
+
+function formatBytes(bytes) {
+  if (bytes === 0) {
+    return '0 B';
+  }
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB']
+  const i = Math.floor(Math.log(bytes) / Math.log(1024))
+  return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
+}
+
+async function shortUrl(url) {
+        let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
+        return await res.text()
+}
