792f5294c535176edaaef9cc42e67655e58fe08a
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-13 20:36:14 -0400

    Actualizar info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,23 +1,64 @@
-import { promises } from 'fs'
-import { join } from 'path'
+let { generateWAMessageFromContent } = (await import(global.baileys)).default 
+import { performance } from 'perf_hooks'
+import fs from 'fs'
+import moment from 'moment-timezone';
+import fetch from 'node-fetch';
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+const res = await fetch('https://api.github.com/repos/Karim-off/GenesisBot-Pro1');
+const json = await res.json();
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var days = Math.floor(seconds / (24 * 60 * 60 * 1000));
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
 
-let handler = async function (m, { conn, __dirname }) {
-let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
-  
-m.reply(`
-» 𝗦 𝗖 𝗥 𝗜 𝗣 𝗧  𝗚 𝗘 𝗡 𝗘 𝗦 𝗜 𝗦
-
-🐈‍⬛ ${fgsc}
-
-_apoyanos con una estrella a nuestro repositorio ⭐_
-
-> ${mssg.ig}
-`.trim())
-    
+  //return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
+  return `🌟  𝗚𝗶𝘁𝗵𝘂𝗯  🌟\n\n*» 𝙉𝙊𝙈𝘽𝙍𝙀 :* ${json?.name || 'GenesisBot'}\n*» 𝙑𝙄𝙎𝙄𝙏𝘼𝙉𝙏𝙀𝙎 :* ${json?.watchers_count || '-'}\n*» 𝘼𝘾𝙏𝙐𝘼𝙇𝙄𝙕𝘼𝘾𝙄𝙊𝙉 :* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n*» 𝙐𝙍𝙇 :* ${json?.html_url || 'https://github.com/karim-off/GenesisBot-Pro1'}\n\n${json?.forks_count || '-'} Forks · ${json?.stargazers_count || '-'} Stars · ${json?.open_issues_count || '-'} Issues\n\n*⏰ 𝙏𝙄𝙀𝙈𝙋𝙊 𝘼𝘾𝙏𝙄𝙑𝙊:*\n \t${pad(days)} Dias\t ${pad(hours)} Horas ${pad(minutes)} Minutos ${pad(seconds)} Segudos \t\n`
 }
+                                        const runtime = process.uptime()
+                            const teks = `${kyun(runtime)}`
+                                        const itsme = `0@s.whatsapp.net`
+                                        const split = `𝗦𝘂𝗽𝗲𝗿 𝗕𝗼𝘁 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽`
+                                        const rtimebro = {
+                                        contextInfo: {
+                                        participant: itsme,
+                                        quotedMessage: {
+                                        extendedTextMessage: {
+                                    text: split
+                                                                        }
+                                                                }
+                                                        }
+                                        }
 
-handler.help = ['script']
+                                                  let prep = generateWAMessageFromContent(m.chat, { orderMessage: { 
+itemCount: -10062007, status: 500,
+surface: 999,
+message: teks,
+description: '^^',
+orderTitle: 'Hi Sis',
+token: '9',
+curreyCode: 'IDR',
+totalCurrencyCode: '>〰<',
+totalAmount1000: '1000000',
+sellerJid: sc,
+thumbnail: 'https://tinyurl.com/22xa95xa'
+}}, {contextInfo: null, quoted: m})
+conn.relayWAMessage(prep)
+//conn.sendMessage(m.chat, `${teks}`, MessageType.text, rtimebro)
+}
+handler.help = ['runtime']
 handler.tags = ['info']
-handler.command = ['sc', 'git', 'script'] 
+handler.command = /^(runtime|sc|activo)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.admin = false
+handler.botAdmin = false
+handler.fail = null 
 
 export default handler
