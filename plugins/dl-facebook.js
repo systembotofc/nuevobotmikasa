05e311b2a5cbd62c801e1bd4f56a270438ddeb9d
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-15 01:57:19 -0400

    Actualizar dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -1,20 +1,93 @@
+/*
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, command, args, usedPrefix }) => {
+  const linknya = args[0]
+
+  if (!args[0]) throw `Input *URL*`
+  m.reply(wait)
+  try {
+    let res = await fetch(`https://api.ryzendesu.com/api/dowloader/fbdown?url=${encodeURIComponent(linknya)}&apikey=${global.ryzen}`)
+    let result = await res.json()
+    let video = result.result.HD
+    //let audio = result.data.audio
+    let cap = global.wm
+    conn.sendMessage(m.chat, { video: { url: video }, caption: cap }, m)
+    //conn.sendMessage(m.chat, { audio: { url: audio }, mimetype: 'audio/mp4' }, { quoted : m })
+  } catch (e) {
+    console.log(e)
+    m.reply(`Fitur error atau Otak pengguna error`)
+  }
+}
+*/
+
 import axios from 'axios'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Input URL!\n\nContoh:\n${usedPrefix + command} https://fb.watch/ksK8U7wgHY/?mibextid=GXl451 hd`
-    if (!args[1]) throw `Input Resolusi!\n\nContoh:\n${usedPrefix + command} https://fb.watch/ksK8U7wgHY/?mibextid=GXl451 hd`
-    if (!/hd|sd|HD|SD/i.test(args[1])) throw 'Hanya Tersedia Kualitas SD Dan HD'
-    let res = await axios.get(API('lol', '/api/facebook', { url: args[0] }, 'apikey'))
-    if (/hd|HD|Hd/i.test(args[1])) {
-        m.reply(wait)
-        conn.sendFile(m.chat, res.data.result[0], '', '', m, false)
+import fetch from 'node-fetch'
+
+var handler = async (m, { args }) => {
+    if (!args[0]) {
+        throw 'Input *URL*';
     }
-    if (/sd|SD|Sd/i.test(args[1])) {
-        m.reply(wait)
-        conn.sendFile(m.chat, res.data.result[1], '', '', m, false)
+    
+    try {
+        const url = args[0];
+        const headersList = {
+            "Accept": "*/*",
+            "User-Agent": "Thunder Client (https://www.thunderclient.com)"
+        };
+
+        const reqOptions = {
+            url: `https://backend.shirokamiryzen.repl.co/fb?u=${url}`,
+            method: "GET",
+            headers: headersList,
+        };
+
+        const response = await axios.request(reqOptions);
+        const firstUrls = response.data.map(item => item.split(','));
+
+        const hdMedia = firstUrls[0][0];
+        const sdMedia = firstUrls[1][0];
+        
+        const hdCaption = `Video Kualitas HD\nLink HD: ${hdMedia}`;
+        const sdCaption = `Video Kualitas SD\nLink SD: ${sdMedia}`;
+        
+        m.reply(wait);
+
+        try {
+            // Send HD video
+            const hdFile = await fetch(hdMedia);
+            conn.sendFile(m.chat, await hdFile.buffer(), 'video_hd.mp4', hdCaption, m);
+
+            try {
+                // Send SD video
+                const sdFile = await fetch(sdMedia);
+           //     conn.sendFile(m.chat, await sdFile.buffer(), 'video_sd.mp4', sdCaption, m);
+            } catch {
+                // If SD video sending fails, no further action needed
+            }
+        } catch {
+            try {
+                // Send SD video
+                const sdFile = await fetch(sdMedia);
+           //     conn.sendFile(m.chat, await sdFile.buffer(), 'video_sd.mp4', sdCaption, m);
+            } catch {
+                // If both HD and SD videos don't exist, send an error message
+                const cap = 'Gagal mengunduh video FB';
+                conn.sendFile(m.chat, 'facebook.mp4', 'facebook.mp4', cap, m);
+            }
+        }
+    } catch {
+        // Jika terjadi kesalahan pada tahap lainnya, kirim pesan kesalahan
+        const cap = 'Gagal mengunduh video FB';
+        conn.sendFile(m.chat, 'facebook.mp4', 'facebook.mp4', cap, m);
     }
 }
-handler.help = ['facebook'].map(v => v + ' <url>')
+
+handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^((facebook|fb)(downloder|dl)?)$/i
 handler.limit = true
+handler.register = true
+
+handler.command = /^(fb(dl)?)$/i
+
 export default handler
