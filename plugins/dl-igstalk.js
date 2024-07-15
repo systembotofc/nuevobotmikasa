91e3132ef1cd8b6dd22aacc421145a3d2edea6c4
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-15 02:15:32 -0400

    Actualizar dl-igstalk.js

--- plugins/dl-igstalk.js
+++ plugins/dl-igstalk.js
@@ -1,103 +1,65 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
 
-import fetch from 'node-fetch';
-import axios from 'axios';
-import cheerio from 'cheerio';
+let handler = async (m, { conn, args }) => {
+  if (!(/^([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)$/i.test(args[0]))) throw 'Masukan username tanpa @'
+  let res = await igstalk(args[0])
+  let json = JSON.parse(JSON.stringify(res))
+  let {
+    fullname,
+    username,
+    posts,
+    postsM,
+    followers,
+    followersM,
+    followingM,
+    following,
+    bio,
+    photo_profile,
+  } = json
+  let caption = `
+*Nama :* ${fullname} 
+*Usernamee :* (@${username})
+*Link :* instagram.com/${username}
+*Post(s) :* ${posts} (${postsM})
+*Following :* ${following} (${followingM})
+*Followers :* ${followers} (${followersM})
+*Bio :*
+${bio}
+`.trim()
+  conn.sendFile(m.chat, photo_profile, 'ppig.jpg', caption, m)
+}
+handler.help = ['igstalk <username>']
+handler.tags = ['search']
 
-async function igstalk(Username) {
+handler.command = /^(igstalk)$/i
+
+module.exports = handler
+
+async function igstalk(username) {
   try {
-    const trResponse = await axios.get(`https://tr.deployers.repl.co/igstalk?user=${Username}`);
-    if (trResponse.status === 200) {
-      return trResponse.data;
-    } else {
-      throw new Error('Failed to fetch Instagram profile');
-    }
-  } catch (error) {
-    try {
-      const dumpoirResponse = await axios.get(`https://dumpoir.com/v/${Username}`, {
-        headers: {
-          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0'
-        }
-      });
-
-      const $ = cheerio.load(dumpoirResponse.data);
-      const result = {
-        bio: $('#user-page > div.user > div > div.col-md-5.my-3 > div').text().trim(),
-        credits: 'Xnuvers007 (https://github.com/Xnuvers007)',
-        followers: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li:nth-child(2)')
-          .text()
-          .replace(' Followers', '')
-          .trim(),
-        following: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li:nth-child(3)')
-          .text()
-          .replace(' Following', '')
-          .trim(),
-        fullname: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > div > a > h1').text().trim(),
-        post: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li:nth-child(1)')
-          .text()
-          .replace(' Posts', '')
-          .trim(),
-        profile: $('#user-page > div.user > div.row > div > div.user__img')
-          .attr('style')
-          .replace(/(background-image: url\(\'|\'\);)/gi, '')
-          .trim(),
-        status: 200,
-        url: `https://www.instagram.com/${Username.replace('@', '')}`,
-        username: `@${Username}`
-      };
-
-      return result;
-    } catch (e) {
-      if (e.response?.status === 404) {
-        throw new Error('Error: Akun tidak ditemukan');
-      } else if (e.response?.status === 403) {
-        throw new Error('Error: Akunnya Di Private');
-      } else {
-        throw new Error('Error: Failed to fetch Instagram profile');
+    const { data } = await axios.get(`https://dumpor.com/v/${username}`, {
+      headers: {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+        "cookie": "yuidss=27908061654918351; yandexuid=27908061654918351; i=xf9t861U10TjlsxcMQGq4rqc85SnhmZXW46mrbxRRXjX2rfo/Es3LHvowEdjFYEfH8aiGdWaL9hOub5AfS5bZf1fa/Q=; ymex=1970331113.yrts.1654971113#1970331112.yrtsi.1654971112; yabs-sid=138805171656519319"
       }
+    })
+    const $ = cheerio.load(data)
+    const results = {
+      username: ($('#user-page > div.user > div.row > div > div.user__title > h4').text() || '').replace(/@/gi, '').trim(),
+      fullname: $('#user-page > div.user > div.row > div > div.user__title > a > h1').text(),
+      photo_profile: ($('#user-page > div.user > div.row > div > div.user__img').attr('style') || '').replace(/(background-image: url\(\'|\'\);)/gi, '').trim(),
+      bio: $('#user-page > div.user > div.row > div > div.user__info-desc').text(),
+      followers: ($('#user-page > div.user > div.row > div > ul > li').eq(1).text() || '').replace(/Followers/gi, '').trim(),
+      followersM: ($('#user-page > div.container > div > div > div:nth-child(1) > div > a').eq(2).text() || '').replace(/Followers/gi, '').trim(),
+      following: ($('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li').eq(2).text() || '').replace(/Following/gi, '').trim(),
+      followingM: ($('#user-page > div.container > div > div > div:nth-child(1) > div > a').eq(3).text() || '').replace(/Following/gi, '').trim(),
+      posts: ($('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li').eq(0).text() || '').replace(/Posts/gi, '').trim(),
+      postsM: ($('#user-page > div.container > div > div > div:nth-child(1) > div > a').eq(0).text() || '').replace(/Posts/gi, '').trim()
     }
+    return results
+  } catch (e) {
+    console.error(e)
+    throw 'Username tidak ditemukan'
   }
 }
-
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `Example use ${usedPrefix}${command} username`;
-
-  try {
-    let res = await igstalk(args[0]);
-
-    let username = res.username;
-    let fullname = res.fullname;
-    let post = res.post;
-    let followe = res.followers;
-    let followi = res.following;
-    let bio = res.bio;
-    let pepe = res.profile;
-    let url = res.url;
-    let credits = res.credits;
-
-    let data = `
-💌 ᴜsᴇʀɴᴀᴍᴇ » 「 ${username} 」
-📧 ғᴜʟʟɴᴀᴍᴇ » 「 ${fullname} 」
-🎏 ${followe}  ғᴏʟʟᴏᴡᴇʀs
-🎀 ${followi} ғᴏʟʟᴏᴡɪɴɢ
-📝 ᴘᴏsᴛ ${post}
-📑 Bɪᴏ: ${bio}
-☣ url ${url}
-☣ Credits By ${credits}
-`.trim();
-
-if (pepe) {
-  let pp = await (await fetch(pepe)).buffer();
-  conn.sendFile(m.chat, pp, 'profile.jpg', data, m);
-} else {
-  conn.reply(m.chat, data, m);
-}
-} catch (error) {
-conn.reply(m.chat, error.message, m);
-}
-};
-
-handler.help = ['igstalk'].map(v => v + ' <username>');
-handler.tags = ['tools'];
-handler.command = /^(igstalk)$/i;
-
-export default handler;
