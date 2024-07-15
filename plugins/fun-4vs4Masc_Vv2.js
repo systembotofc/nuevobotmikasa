79b7fcb1840cde525e6ac7dab05f8894412bb414
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-15 14:39:53 -0400

    Actualizar plugins

---
+++ plugins/fun-4vs4Masc_Vv2.js
@@ -0,0 +1,43 @@
+var handler = async (m, { conn, participants, groupMetadata, args, text }) => {
+
+const pp = 'https://tinyurl.com/2648ennm'
+const groupAdmins = participants.filter(p => p.admin)
+const listaAdmins = groupAdmins.map((v, i) => ``).join('\n')
+const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
+if (!text) return m.reply('➤ `𝗔𝗩𝗜𝗦𝗢` 🐈‍⬛\n\n*INGRESA UN HORARIO*\n_Ejemplo: .4vs4infimasc 10:00 🇦🇷_')
+if (text.length < 0) return m.reply(`⚙️ 𝗛𝗢𝗥𝗔𝗥𝗜𝗢 𝗠𝗔𝗟 𝗘𝗦𝗖𝗥𝗜𝗧𝗢, 𝗜𝗡𝗧𝗘𝗡𝗧𝗔 𝗗𝗘 𝗡𝗨𝗘𝗩𝗢.`)
+        m.react('🎮') 
+let mensaje = args.join` `
+let yo = `│🕓 𝗛𝗢𝗥𝗔: *${text}*`
+let texto = `
+╭──────>⋆☽⋆ 🐈‍⬛ ⋆☾⋆<──────╮
+ㅤ          •𝟰  𝗩 𝗘 𝗥 𝗦 𝗨 𝗦  𝟰•
+                    ${groupMetadata.subject}
+╰──────>⋆☽⋆ 🐈‍⬛ ⋆☾⋆<──────╯
+
+
+╭──────>⋆☽⋆ 🐈‍⬛ ⋆☾⋆<──────╮
+${yo}
+│🎮 𝗠𝗢𝗗𝗔𝗟𝗜𝗗𝗔𝗗: *vivido 😼*
+│
+│     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 
+│
+│🫧 ➤ 
+│🫧 ➤ 
+│🫧 ➤ 
+│🫧 ➤ 
+│
+│ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
+│🫧 ➤ 
+│🫧 ➤ 
+╰──────>⋆☽⋆ 🐈‍⬛ ⋆☾⋆<──────╯`.trim()
+conn.sendFile(m.chat, pp, 'error.jpg', texto, m, true, { mentions: [...groupAdmins.map(v => v.id), owner] })
+
+}
+handler.help = ['4vs4compemasc <hora>']
+handler.tags = ['ff']
+handler.command = /^(4x4compemasc|4vs4compemasc)$/i
+handler.admin = true
+handler.group = true
+
+export default handler
