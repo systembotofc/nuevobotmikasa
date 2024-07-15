5aa6f92542ce4e7ffb9010b23f1cae9e16bf473c
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-15 00:35:42 -0400

    Actualizar dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -1,34 +1,24 @@
+
 import fg from 'api-dylux';
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
-const handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) conn.reply(m.chat, `*𝙸𝚗𝚐𝚛𝚎𝚜𝚊 𝚞𝚗 𝚎𝚗𝚕𝚊𝚌𝚎 𝚍𝚎 𝙵𝚊𝚌𝚎𝚋𝚘𝚘𝚔 :𝟹*\n\n*_𝚎𝚓𝚎𝚖𝚙𝚕𝚘_*\n*${usedPrefix + command}* https://www.facebook.com/Ankursajiyaan/videos/981948876160874/?mibextid=rS40aB7S9Ucbxw6v`, m)
-
-  const urlRegex = /^(?:https?:\/\/)?(?:www\.)?(?:facebook\.com|fb\.watch)\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
-  if (!urlRegex.test(args[0])) conn.reply(m.chat,'𝙿𝚘𝚛𝚏𝚊𝚟𝚘𝚛 𝚛𝚎𝚟𝚒𝚜𝚊 𝚀𝚞𝚎 𝙴𝚕 𝙻𝚒𝚗𝚔 𝚂𝚎𝚊 𝙳𝚎 𝙵𝚊𝚌𝚎𝚋𝚘𝚘𝚔 :𝟹', m)
-
-  m.react(🕒);
+if (!args[0]) throw `✳️ ${mssg.noLink('Facebook')}\n\n📌 ${mssg.example} :\n*${usedPrefix + command}* https://fb.watch/d7nB8-L-gR/`
+  m.react(rwait);
 
   try {
-    const result = await fg.fbdl(args[0]);
-    const tex = `
-⊱ ─── { *𝚁𝙴𝙼 - 𝙵𝙱* } ─── ⊰
-↳ *𝚃𝚒𝚝𝚞𝚕𝚘:* ${result.title}
-⊱ ────── {⋆♬⋆} ────── ⊰`;
-
-    const response = await fetch(result.videoUrl);
-    const arrayBuffer = await response.arrayBuffer();
-    const videoBuffer = Buffer.from(arrayBuffer);
-
-    conn.sendFile(m.chat, videoBuffer, 'fb.mp4', tex, m, null, rcanal);
+    let result = await fg.fbdl(args[0]);
+    let tex = `
+┌─⊷ *FBDL*
+▢ *${mssg.title}:* ${result.title}
+└───────────`;
+    conn.sendFile(m.chat, result.videoUrl, 'fb.mp4', tex, m);
     m.react(done);
   } catch (error) {
-    console.log(error);
-    m.reply('⚠️ Se produjo un error al procesar la solicitud. Por favor, inténtelo de nuevo más tarde.');
+    m.reply(mssg.error)
   }
 };
-
-handler.help = ['facebook <url>'];
-handler.tags = ['downloader'];
+handler.help = ['facebook'].map(v => v + ' <url>');
+handler.tags = ['dl'];
 handler.command = /^((facebook|fb)(downloder|dl)?)$/i;
 handler.diamond = true;
 
