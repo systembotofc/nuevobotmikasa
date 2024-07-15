519c7a1095e6cbb3b66285344daab00455d77c98
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-15 00:51:50 -0400

    Actualizar dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -7,7 +7,7 @@ var handler = async (m, { conn, args }) => {
     }
 
     try {
-        await conn.reply(m.chat, 'Tunggu sebentar kak, video sedang di download... server 1', m);
+        await conn.reply(m.chat, 'Espera un momento hermana, el video se está descargando... servidor 1', m);
 
         const tiktokData = await tryServer1(args[0]);
 
