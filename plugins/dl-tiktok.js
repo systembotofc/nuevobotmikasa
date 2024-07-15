8ee47df82efe8d924a3ed9a1b73e1f192b2e1fff
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-15 00:59:25 -0400

    Actualizar dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -30,7 +30,7 @@ var handler = async (m, { conn, args }) => {
         if (videoURL || videoURLWatermark) {
             await conn.sendFile(m.chat, videoURL, 'tiktok.mp4', `${infonya_gan}`, m);
             setTimeout(async () => {
-                //await conn.sendFile(m.chat, videoURLWatermark, 'tiktokwm.mp4', `${infonya_gan}`, m, fwc);
+                //await conn.sendFile(m.chat, videoURLWatermark, 'tiktokwm.mp4', `${infonya_gan}`, m);
                 //await conn.sendFile(m.chat, `${tiktokData.music.play_url}`,'lagutt.mp3', 'ini lagunya',m);
             }, 5000);
         } else {
