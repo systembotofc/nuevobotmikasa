6451e3826549ac596f36b5f78b06e7e98879c634
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-19 11:28:54 -0400

    Actualizar Prueba5.js

--- plugins/Prueba5.js
+++ plugins/Prueba5.js
@@ -1,7 +1,8 @@
 let handler  = async (m, { conn, usedPrefix, command }) => {
 let res = await tiktokfrases[Math.floor(Math.random() * tiktokfrases.length)]
-await m.react('😁')
-conn.sendMessage(m.chat, { video: { url: res }, caption: `» 𝙍𝘼𝙉𝘿𝙊𝙈𝙎 𝙏𝙄𝙆 𝙏𝙊𝙆 😬` }, { quoted: m })}
+await m.react('♻️')
+await conn.sendButton(m.chat, '» 𝙍𝘼𝙉𝘿𝙊𝙈𝙎 𝙏𝙄𝙆 𝙏𝙊𝙆 😬', packname, url, [['🥱 𝐒𝐈𝐆𝐔𝐈𝐄𝐍𝐓𝐄 🐈‍⬛', `/${command}`]], null, null, m)  
+
 handler.help = ['tiktokfrases']
 handler.tags = ['random']
 handler.command = /^(tiktokrandom)$/i
